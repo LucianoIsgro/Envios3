6d73defe5f661c5020d824b6235ea171fdcac634
LucianoIsgro <lucianisgro@hotmail.com>
2022-06-23 11:11:08 -0300

    primer commit

---
+++ src/main/java/enviosPackage/Envios3Application.java
@@ -0,0 +1,13 @@
+package enviosPackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Envios3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Envios3Application.class, args);
+	}
+
+}
