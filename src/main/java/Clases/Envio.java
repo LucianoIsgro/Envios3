9e877d6012934722af1a7ff92581c1662f4baaff
LucianoIsgro <lucianisgro@hotmail.com>
2022-06-23 12:51:27 -0300

    A ver si el test tira error

--- src/main/java/Clases/Envio.java
+++ src/main/java/Clases/Envio.java
@@ -1,7 +1,7 @@
 package Clases;
 
 public class Envio {
-	 public static String codigoEnvio = "10A34JF142";
+	 public static String codigoEnvio = "10A34J142";
 	 public static String[] observaciones = {"Observacion1", "a", "b"};
 	 public static String estado = "Pendiente";
 	 public static double precio = 800.17;
