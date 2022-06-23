ca6a22261001381d2378be3ed893ce5ac30b047a
LucianoIsgro <lucianisgro@hotmail.com>
2022-06-23 12:13:07 -0300

    Dale

--- src/test/java/enviosPackage/Envios3ApplicationTests.java
+++ src/test/java/enviosPackage/Envios3ApplicationTests.java
@@ -1,7 +1,11 @@
 package enviosPackage;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import Clases.Envio;
 
 @SpringBootTest
 class Envios3ApplicationTests {
@@ -9,5 +13,15 @@ class Envios3ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	
+	@Test
+	void validarLongitudCodigoEnvio() {
+		Integer longitud = Envio.codigoEnvio.length();
+		assertEquals(longitud, 10);
+	}
+	
+	
+	
 
 }
